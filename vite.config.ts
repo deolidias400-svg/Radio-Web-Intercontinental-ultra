c07090a03addaf350f6e8f124fab219199db4f12
deolidias400-svg <deolidias400@gmail.com>
2026-06-09 20:10:01 -0300

    Ajuste de base para relativo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Radio-Web-Intercontinental-ultra/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
